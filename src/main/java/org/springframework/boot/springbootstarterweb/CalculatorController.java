34d20ce87b136361e2482aaee4217f2c7ef704bf
MalinkaGaming <anutka512@yandex.ru>
2024-02-18 22:27:09 +0300

    готова домашка

--- src/main/java/org/springframework/boot/springbootstarterweb/CalculatorController.java
+++ src/main/java/org/springframework/boot/springbootstarterweb/CalculatorController.java
@@ -6,7 +6,6 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class CalculatorController {
-
     @GetMapping("/calculator")
     public String welcome() {
         return "Добро пожаловать в калькулятор";
